5581ebf83b0d5c670037eed2ea68f2580c8e77a3
allocblock <690051933@qq.com>
2020-04-20 21:05:17 +0800

    add fft recur implement

--- include/noise_graph_generator.h
+++ include/noise_graph_generator.h
@@ -13,8 +13,10 @@
 #include <ctime>
 #include <fstream>
 #include <algorithm>
+#include <complex>
 
 #include "random.h"
+#define PI 3.14159265358979
 
 /* debug */
 #include <iostream>
@@ -172,6 +174,93 @@ public:
             }
         }
     }
+
+    complex<float> omega(int k, int n) {
+        float base = (float)k / n * 2 * PI;
+        return complex<float>(cos(base), sin(base));
+    }
+
+    void fftRecur(complex<float>* v, int n) {
+        if (n == 1) {
+            return;
+        }
+        // 切换顺序
+        int m = n / 2;
+        complex<float>* buf = new complex<float>[n];
+        for (int i = 0; i < m; i++) {
+            buf[i] = v[2 * i];
+            buf[i + m] = v[2 * i + 1];
+        }
+        // 递归计算
+        fftRecur(buf, m);
+        fftRecur(buf + m, n - m);
+        // 并归
+        for (int k = 0; k < n / 2; k++) {
+            complex<float> A1k = buf[k];
+            complex<float> A2k = buf[k + m];
+            buf[k] = A1k + omega(k, n) * A2k;
+            buf[k + m] = A1k - omega(k, n) * A2k;
+        }
+        // 填入
+        for (int i = 0; i < n; i++) {
+            v[i] = buf[i];
+        }
+    }
+
+    GrayImage spectrum() {
+        complex<float>* result = new complex<float>[w * h];
+
+        for (int j = 0; j < h; j++) { // 先对每行
+            // 填入
+            complex<float>* v = new complex<float>[w];
+            for (int i = 0; i < w; i++) {
+                v[i] = complex<float>((float)pixel(i, j) / 255, 0.0f); // [0,1]
+            }
+            // 计算
+            fftRecur(v, w);
+            // 保存结果
+            for (int i = 0; i < w; i++) {
+                result[j * w + i] = v[i];
+            }
+        }
+        for (int i = 0; i < w; i++) { // 再对每列
+            // 填入
+            complex<float>* v = new complex<float>[h];
+            for (int j = 0; j < h; j++) {
+                v[j] = result[j * w + i];
+            }
+            // 计算
+            fftRecur(v, h);
+            
+            // 保存结果
+            for (int j = 0; j < h; j++) {
+                result[j * w + i]  = v[j];
+            }
+        }
+        
+        // 计算长度，并映射到[0,1]
+        float* resultLen = new float[w * h];
+        float maxFreqValue = 0;
+        for (int i = 0; i < w*h; i++) {
+            float len = sqrt(result[i].real() * result[i].real() + result[i].imag() * result[i].imag());
+            len = log(len) + 1; // 0Hz频率会很高，是因为取值为[0,1]，为减小差距取log，图像更直观
+            maxFreqValue = max(maxFreqValue, len);
+            resultLen[i] = max(0.0f, len);
+        }
+        for (int i = 0; i < w * h; i++) {
+            resultLen[i] /= maxFreqValue; // 缩放
+        }
+        // 填入
+        GrayImage resImg(w, h);
+        for (int j = 0; j < h; j++) {
+            for (int i = 0; i < w; i++) {
+                byte color = intToByte((int)(resultLen[j * w + i] * 255));
+                resImg.pixel((i + w/2) % w, (j + h / 2) % h, color); // 向右下方循环平移半张图片
+            }
+        }
+        return resImg;
+    }
+    
 };
 
 
--- main.cpp
+++ main.cpp
@@ -15,32 +15,40 @@ void runTests() {
     cout << "生成 1x1 白噪声(White Noise) 图像...";
     img = &whiteNoise(width, height, 1, seed);
     savePPMP2(*img, "white_noise_1x1.ppm");
+    savePPMP2(img->spectrum(), "white_noise_1x1_spectrum.ppm");
     cout << "\r生成 1x1 白噪声(White Noise) 图像完成" << endl;
 
     // 白噪声，16x16
     cout << "生成 16x16 白噪声(White Noise) 图像...";
     img = &whiteNoise(width, height, 16, seed);
     savePPMP2(*img, "white_noise_16x16.ppm");
+    savePPMP2(img->spectrum(), "white_noise_16x16_spectrum.ppm");
     cout << "\r生成 16x16 白噪声(White Noise) 图像完成" << endl;
 
     // 连续噪声，16x16
     cout << "生成 16x16 值噪声(Value Noise) 图像...";
     img = &valueNoise(width, height, 16, seed);
     savePPMP2(*img, "value_noise_16x16.ppm");
+    savePPMP2(img->spectrum(), "value_noise_16x16_spectrum.ppm");
     cout << "\r生成 16x16 值噪声(Value Noise) 图像完成" << endl;
+    
 
     // 柏林噪声，16x16
     cout << "生成 16x16 柏林噪声(Perlin Noise) 图像...";
     img = &perlinNoise(width, height, 16, seed);
     savePPMP2(*img, "perlin_noise_16x16.ppm");
+    savePPMP2(img->spectrum(), "perlin_noise_16x16_spectrum.ppm");
     cout << "\r生成 16x16 柏林噪声(Perlin Noise) 图像完成" << endl;
-
+    
+    
     // 柏林噪声，16x16+量化
     cout << "生成 64x64 柏林噪声(Perlin Noise)+量化 图像...";
     img = &perlinNoise(width, height, 64, seed);
     img->quantify(10);
     savePPMP2(*img, "perlin_noise_64x64_quantify_10.ppm");
+    savePPMP2(img->spectrum(), "perlin_noise_64x64_spectrum.ppm");
     cout << "\r生成 64x64 柏林噪声(Perlin Noise)+量化 图像完成" << endl;
+
     
     // 值噪声，分形
     cout << "生成 分形+值噪声 图像...";
@@ -58,6 +66,7 @@ void runTests() {
         octave--;
     }
     savePPMP2(*img, "fabric_value_noise.ppm");
+    savePPMP2(img->spectrum(), "fabric_value_noise_spectrum.ppm");
     cout << "\r生成 分形+值噪声 图像完成" << endl;
     
     // 柏林噪声，分形
@@ -76,17 +85,20 @@ void runTests() {
         octave--;
     }
     savePPMP2(*img, "fabric_perlin_noise.ppm");
+    savePPMP2(img->spectrum(), "fabric_perlin_noise_spectrum.ppm");
     cout << "\r生成 分形+柏林噪声 图像完成" << endl;
     
 
     cout << "生成 16x16 单形噪声(Simplex Noise) 图像...";
     img = &simplexNoise(width, height, 16, seed);
     savePPMP2(*img, "simplex_noise_16x16.ppm");
+    savePPMP2(img->spectrum(), "simplex_noise_16x16_spectrum.ppm");
     cout << "\r生成 16x16 单形噪声(Simplex Noise) 图像完成" << endl;
     
     cout << "生成 16x16 沃利噪声(Worley Noise) 图像...";
     img = &worleyNoise(width, height, 16, seed);
     savePPMP2(*img, "worley_noise_16x16.ppm");
+    savePPMP2(img->spectrum(), "worley_noise_16x16_spectrum.ppm");
     cout << "\r生成 16x16 沃利噪声(Worley Noise) 图像完成" << endl;
     
 }
